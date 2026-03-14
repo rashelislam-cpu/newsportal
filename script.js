c5cc46977acf8ceeb6753a88bcce6d4c0869a1aa
rashelislam-cpu <rashel222434@gmail.com>
2026-03-15 01:04:34 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const newspapers = [
+
+{
+name: "Prothom Alo",
+link: "https://www.prothomalo.com",
+logo: "image/d:\\News_portal\\image.png"
+},
+
+{
+name: "Daily Star",
+link: "https://www.thedailystar.net",
+logo: "image/d:\\239.md.jpg\\dailystar.png"
+},
+
+{
+name: "BBC News",
+link: "https://www.bbc.com",
+logo: "image/d:\\News_portal\\bbc.png"
+},
+
+{
+name: "CNN",
+link: "https://www.cnn.com",
+logo: "image/d:\\News_portal\\cnn.png"
+}
+
+];
